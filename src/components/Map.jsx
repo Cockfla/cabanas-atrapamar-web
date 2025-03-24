aa6393ae6e3d105914fe76236a2e39267f9b1ce2
Cockfla <eliasbaha100@gmail.com>
2025-03-24 18:54:38 -0300

    iconos leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,18 @@
+// Map.astro
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"; // Importa los estilos de Leaflet
+import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+
+// Solución para el icono del marcador
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
 
 export default function Map() {
   return (
@@ -8,13 +20,20 @@ export default function Map() {
       center={[-34.39491810022256, -72.0175572211526]}
       zoom={16}
       scrollWheelZoom={false}
-      style={{ height: "400px", width: "100%" }} // Define un tamaño fijo para el mapa
+      style={{ height: "400px", width: "100%" }}
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
       <Marker position={[-34.39491810022256, -72.0175572211526]}>
         <Popup>
           <div className="flex flex-col items-center justify-center">
-            <img src="/cabanas-atrapamar-log.png" width={50} />
+            <img
+              src="/cabanas-atrapamar-log.png"
+              width={50}
+              alt="Logo Cabañas Atrapa Mar"
+            />
             <b>Cabañas Atrapa Mar</b>
           </div>
         </Popup>

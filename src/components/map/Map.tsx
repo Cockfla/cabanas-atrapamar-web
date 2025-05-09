b07d93beee9b9e7697d9e11c859ba7c065b565a8
ericpedreros <ERIC.PEDREROS@correoaiep.cl>
2025-05-08 23:47:27 -0400

    feat: agregar icono personalizado al componente Map y corregir importaciones

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,22 +1,11 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-
-// Solución para el icono del marcador
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-});
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import { customIcon } from './MarkerIcon' // Importar el icono personalizado
 
 interface Props {
-  lat: number;
-  lng: number;
-  titulo: string;
+  lat: number
+  lng: number
+  titulo: string
 }
 
 export default function Map({ lat, lng, titulo }: Props) {
@@ -25,14 +14,16 @@ export default function Map({ lat, lng, titulo }: Props) {
       center={[lat, lng]}
       zoom={16}
       scrollWheelZoom={false}
-      style={{ height: "400px", width: "100%" }}
+      style={{ height: '400px', width: '100%' }}
       className="z-0"
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[lat, lng]}>
+      <Marker position={[lat, lng]} icon={customIcon}>
+        {' '}
+        {/* Usa el icono personalizado */}
         <Popup>
           <div className="flex flex-col items-center justify-center">
             <img
@@ -45,5 +36,5 @@ export default function Map({ lat, lng, titulo }: Props) {
         </Popup>
       </Marker>
     </MapContainer>
-  );
+  )
 }
